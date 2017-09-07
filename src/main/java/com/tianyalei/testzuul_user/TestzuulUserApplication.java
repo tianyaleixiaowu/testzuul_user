b95db1bfdf082debb0406576dd4aad5d73a2b06f
wuwf <272551766@qq.com>
2017-09-07 16:40:24 +0800

    a

---
+++ src/main/java/com/tianyalei/testzuul_user/TestzuulUserApplication.java
@@ -0,0 +1,12 @@
+package com.tianyalei.testzuul_user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestzuulUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestzuulUserApplication.class, args);
+	}
+}
